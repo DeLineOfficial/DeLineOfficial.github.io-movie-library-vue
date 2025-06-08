f4d4f342ecd17e23aaf7da5d1a40703e1bec8a15
DeLineOfficial <artem.stolyar.2002@mail.ru>
2025-06-08 17:34:58 +0300

    Стили

--- src/features/home-module/home-module.vue
+++ src/features/home-module/home-module.vue
@@ -29,8 +29,20 @@ onMounted(() => {
             </div>
         </div>
 
-        <div class="omdb__content">
-            <div v-for="item in movieStore.movies">{{ item }}</div>
+        <div class="omdb__content" v-if="movieStore.movies.length">
+            <div v-for="item in movieStore.movies" :key="item.id" class="omdb__item">
+                <img :src="item.Poster" alt="img" @click="console.log(item)">
+                
+                <div class="item__description">
+                    <p class="title">{{ item.Title }}</p>
+                    <p class="year">{{ item.Year }}</p>
+    
+                    <a :href="`https://www.imdb.com/title/${item.imdbID}/`" target="_blank" class="to">Перейти</a>
+                </div>
+            </div>
+        </div>
+        <div class="omdb__content not-found" v-else>
+            В данный момент ничего не найдено.
         </div>
     </div>
 </template>
@@ -48,7 +60,10 @@ onMounted(() => {
         width: 100%;
         justify-content: space-between;
         align-items: center;
-
+        margin-bottom: 20px;
+        > h1 {
+            color: var(--text-primary);
+        }
         > .omdb__control {
             display: flex;
             align-items: center;
@@ -69,8 +84,54 @@ onMounted(() => {
         display: flex;
         width: 100%;
         height: 100%;
-        flex-direction: column;
-        overflow: auto;
+        flex-wrap: wrap;
+        overflow-y: auto;
+        overflow-x: hidden;
+        gap: 20px;
+        padding-right: 16px;
+        justify-content: space-between;
+        
+        &.not-found {
+            align-items: center;
+            color: var(--text-primary);
+            justify-content: center;
+        }
+        > .omdb__item {
+            display: flex;
+            flex-direction: column;
+            height: 600px;
+            width: calc(20% - 20px);
+            background: var(--background-secondary);
+
+            > img {
+                width: 100%;
+                height: 440px;
+                object-fit: cover;
+                object-position: center;
+            }
+
+            > .item__description {
+                padding: 20px;
+                display: flex;
+                flex-direction: column;
+                height: 160px;
+
+                > .title {
+                    font-size: 24px;
+                    color: var(--text-primary);
+                }
+                > .year {
+                    margin-top: 4px;
+                    font-size: 12px;
+                    color: var(--text-disable);
+                    margin-bottom: 20px;
+                }
+                > .to {
+                    display: flex;
+                    margin-top: auto;
+                }
+            }
+        }
     }
 }
 
